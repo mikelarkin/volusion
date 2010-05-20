8f5876cba5f8723b7ebb5afec69dee6bc7309f64
Michael Larkin <mikelarkin@pixellent.com>
2010-05-20 10:16:15 -0400

    pluginified

--- Sample.rb
+++ Sample.rb
@@ -1,5 +1,6 @@
-require 'VolusionAPI.rb'
-api = VolusionAPI.new("http://v420173.49oy7bwf3eth.demo2.volusion.com", "petercm@gmail.com", "8A187A7462347079FA53B4A40DAFCF365F32D2009BD99D913D88534F8C49318E");
+require File.dirname(__FILE__) + '/lib/volusion_api'
+
+api = Volusion::API.new("http://v420173.49oy7bwf3eth.demo2.volusion.com", "petercm@gmail.com", "8A187A7462347079FA53B4A40DAFCF365F32D2009BD99D913D88534F8C49318E");
 
 puts "--------All Orders with status 'Processing'--------"
 orders = api.getOrders({:field => :status, :value => 'Processing'})
---
+++ lib/volusion_api.rb
@@ -0,0 +1,3 @@
+require File.dirname(__FILE__) + '/volusion/api'
+require File.dirname(__FILE__) + '/volusion/line_item'
+require File.dirname(__FILE__) + '/volusion/order'
